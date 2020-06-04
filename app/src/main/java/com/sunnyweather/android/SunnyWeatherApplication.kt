dd2a49f7d497964e442054b9af97f551d500fd09
yuhongjang <1447451994@qq.com>
2020-06-04 10:24:18 +0800

    实现搜索全球城市数据功能。

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -1,4 +1,22 @@
 package com.sunnyweather.android
 
-class SunnyWeatherApplication {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object {
+
+        const val TOKEN = "RMhUY3EoaBvDgbPa" // 填入你申请到的令牌值
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
 }
--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -1,4 +1,21 @@
 package com.sunnyweather.android.ui.place
 
-class PlaceViewModel {
+import androidx.lifecycle.*
+import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.model.Place
+
+class PlaceViewModel : ViewModel() {
+
+    private val searchLiveData = MutableLiveData<String>()
+
+    val placeList = ArrayList<Place>()
+
+    val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
+        Repository.searchPlaces(query)
+    }
+
+    fun searchPlaces(query: String) {
+        searchLiveData.value = query
+    }
+
 }
